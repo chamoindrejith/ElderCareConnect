897b0e465378615aebd8f9e2cbb52f48706fcbdd
DilmyPerera <samadiniperera20@gmail.com>
2024-11-22 15:50:33 +0530

    Define medication reminder schema in medicationReminders.model.js

--- backend/src/controllers/medicationReminder.controller.js
+++ backend/src/controllers/medicationReminder.controller.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+
+const medicationReminderSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    details: { type: String, required: true },
+    createdBy: { type: String, required: true },
+    updatedBy: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  });
