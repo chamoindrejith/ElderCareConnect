33e5efa8c68202587543a11ca48c51c03365bfb0
DilmyPerera <samadiniperera20@gmail.com>
2024-11-22 17:20:38 +0530

    Add MongoDB connection setup with Mongoose

---
+++ backend/src/db/dbconfig.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const url= process.env.MONGODB_URI;
+
+const connectDB = () => {
+    mongoose.connect(url).then(()=>{
+        console.log("DB Connected");
+    }).catch((err)=>{
+        console.log("DB Connection Error: ", err);
+    });
+}
+
+module.exports={
+    connectDB
+} 
