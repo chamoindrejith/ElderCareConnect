13e14fb1567d7edebdb73c134e46ca27b862b1ea
DilmyPerera <samadiniperera20@gmail.com>
2024-11-22 17:56:00 +0530

    Create HTTP Server

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,14 @@ require('dotenv').config();
 const database = require('../backend/src/db/dbconfig.js');
 const routes = require('../backend/src/routes/index.js');
 const port = process.env.PORT;
-
+const http = require('http');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const server = http.createServer(app);
 database.connectDB();
 
 app.use('/api', routes);
