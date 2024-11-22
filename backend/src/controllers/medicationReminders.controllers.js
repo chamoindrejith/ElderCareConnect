b628f8a16a0cd8737c5c1e22cd331027df442764
DilmyPerera <samadiniperera20@gmail.com>
2024-11-22 16:41:35 +0530

    Add createReminder controller for adding new medication reminders

--- backend/src/controllers/medicationReminders.controllers.js
+++ backend/src/controllers/medicationReminders.controllers.js
@@ -1,1 +1,18 @@
 const MedicationReminder = require('../models/medicationReminder.js');
+
+
+exports.createReminder = async (req, res) => {
+    try {
+      const { title, details } = req.body;
+      const reminder = new MedicationReminder({
+        title,
+        details,
+        createdBy: req.user.role,
+        updatedBy: req.user.role
+      });
+      await reminder.save();
+      res.status(201).json(reminder);
+    } catch (error) {
+      res.status(500).json({ message: 'Error creating reminder', error });
+    }
+  };
