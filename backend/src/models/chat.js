8772d8041ae4fc81d47390b9d15d6f4e43901d59
DilmyPerera <samadiniperera20@gmail.com>
2024-11-28 09:57:09 +0530

    Exported `Message` model using the `messageSchema`.

--- backend/src/models/chat.js
+++ backend/src/models/chat.js
@@ -6,3 +6,6 @@ const messageSchema = new mongoose.Schema({
     text: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
   });
+
+module.exports = mongoose.model('Message', messageSchema);
+
