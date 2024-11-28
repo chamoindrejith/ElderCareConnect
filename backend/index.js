95e03775cbd10c07dc2984f67c1dbf16614fcfba
DilmyPerera <samadiniperera20@gmail.com>
2024-11-28 12:12:53 +0530

    Initialized socket.io with the server

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,21 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const database = require('../backend/src/db/dbconfig.js');
 const routes = require('../backend/src/routes/index.js');
+const http = require('http');
 const socketIo = require('socket.io');
 const { initSocket } = require('./src/controllers/chat.controllers.js');
 
 const port = process.env.PORT;
-const http = require('http');
+
+const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const server = http.createServer(app);
+
 database.connectDB();
 
 app.use('/api', routes);
