39f4e9effcdd22c8256ea89a82e8c8e380d6f4fd
DilmyPerera <samadiniperera20@gmail.com>
2024-11-28 10:17:22 +0530

    Route to get chat history between two users

--- backend/src/routes/chat.router.js
+++ backend/src/routes/chat.router.js
@@ -5,3 +5,6 @@ const router = express.Router();
 const chatController = require('../controllers/chat.controllers');
 
 const { authenticateToken } = require('../middleware/auth.middleware');
+
+
+router.get('/history/:senderId/:receiverId', authenticateToken, chatController.getChatHistory);
