eb38c6e20f8df5b6ed7320737b62f7974fa03935
DilmyPerera <samadiniperera20@gmail.com>
2024-11-28 10:21:49 +0530

    Added middleware to use `chatRouter` for `/chat` route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,7 +7,7 @@ const chatRouter = require('../routes/chat.router.js')
 const router = express.Router();
 
 router.use('/medicationReminders', medicationRemindersRouter);
-
+router.use('/chat', chatRouter);
 
 
 module.exports = router;
