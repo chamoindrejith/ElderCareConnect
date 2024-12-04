2eae20e0b6c52a666e231d4c5bd0e81a177dff78
PLChandrasekara <pramudilanka289@gmail.com>
2024-12-04 23:44:46 +0530

    Add update userRoute in user router

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {register} = require('../controllers/userController.js');
 const {login} = require('../controllers/userController.js');
-
+const {updateUser}=require('../controllers/userController.js')
 
 router.post('/register',register);
 router.post('/login',login);
+router.put('/updateUser',updateUser);
 
 
 
