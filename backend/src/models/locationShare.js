3110d515c653811ae2b7f49d92533b2735b16beb
DilmyPerera <samadiniperera20@gmail.com>
2024-12-05 22:05:24 +0530

    Add fields to `locationSchema`

--- backend/src/models/locationShare.js
+++ backend/src/models/locationShare.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
-    
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    location: {
+      latitude: { type: Number, required: true },
+      longitude: { type: Number, required: true },
+    },
+    sharedAt: { type: Date, default: Date.now },
   });
