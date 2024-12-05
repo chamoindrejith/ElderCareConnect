ae51936bb81303562ebdfd8cf93060d89a953c49
PLChandrasekara <pramudilanka289@gmail.com>
2024-12-05 19:24:14 +0530

    Add delete user path in to the userRoute

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const {register} = require('../controllers/userController.js');
 const {login} = require('../controllers/userController.js');
 const {updateUser}=require('../controllers/userController.js');
-
+const {deleteUser}=require('../controllers/userController.js');
 
 router.post('/register',register);
 router.post('/login',login);
 router.put('/updateUser/:userId',updateUser);
-
+router.delete('/deleteUser/:userId',deleteUser);
 
 
 module.exports = router;
