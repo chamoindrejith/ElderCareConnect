71c6ccbf0dd7ed8ebc75e645a14ba0ae61a739b9
DilmyPerera <samadiniperera20@gmail.com>
2024-12-07 19:02:37 +0530

    Clear content of user.js file

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,16 +1,3 @@
-const mongoose =require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true,unique:true },
-  password: { type: String, required: true },
-  email:{type:String,required:true},
-  dateOfBirth: { type: Date },
-  contactInfo: { type: String },
-  address: { type: String },
-  emergencyContactId: { type: mongoose.Schema.Types.ObjectId, ref: 'EmergencyContact' },
-  medicalInfoId: { type: mongoose.Schema.Types.ObjectId, ref: 'HealthRecord' },
-  role: { type: String, enum: ['eldery', 'caregiver'], required:true},
-  lastCheckin: { type: Date, default: Date.now }
-});
 
-module.exports = mongoose.model('User', userSchema);
+
