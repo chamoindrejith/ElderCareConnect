19ed6f6352b13eca1c7023b9617478984cbf228d
DilmyPerera <samadiniperera20@gmail.com>
2024-12-07 19:39:11 +0530

    Handle missing token in authenticateToken middleware with 401 response

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,4 +3,9 @@ const User = require('../models/User');
 
 exports.authenticateToken = async (req, res, next) => {
     const token = req.header('Authorization')?.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({ message: 'Access Denied. No token provided.' });
+    }
+
 };
