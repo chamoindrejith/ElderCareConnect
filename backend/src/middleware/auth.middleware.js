65e84fededd6cff95a20b3b195962f82be5915ba
DilmyPerera <samadiniperera20@gmail.com>
2024-12-07 19:38:38 +0530

    Extract token from Authorization header in authenticateToken middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,5 +2,5 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.authenticateToken = async (req, res, next) => {
-
+    const token = req.header('Authorization')?.split(' ')[1];
 };
