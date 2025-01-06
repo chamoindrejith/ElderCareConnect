2ca5a57a53e33e78d6854bcd254a38f9c1d291e5
Chamodi <chamodiindrejith14@gmail.com>
2025-01-06 12:27:10 +0530

    feat: dashboard cards

--- frontend/healthdashboard/src/app/page.tsx
+++ frontend/healthdashboard/src/app/page.tsx
@@ -1,10 +1,9 @@
 import PageTitle from "@/components/PageTitle";
 
-
 export default function Home() {
   return (
-  <div>
-    <PageTitle title="Dashboard"/>
+  <div className="flex flex-col w-full gap-5">
+    <PageTitle title="Home"/>
   </div>
   );
 }
